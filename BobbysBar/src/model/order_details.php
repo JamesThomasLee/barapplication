d174c2af726e8e1f745b9549c89e9f2f7a834173
James <james.lee-1@students.plymouth.ac.uk>
2019-12-22 11:17:00 +0000

    created order details class

---
+++ BobbysBar/src/model/order_details.php
@@ -0,0 +1,30 @@
+<?php
+
+class order_details
+{
+    private $order_id;
+    private $product_id;
+    private $quantity;
+
+    public function __construct($order_id ,$product_id, $quantity)
+    {
+        $this->order_id = $order_id;
+        $this->product_id = $product_id;
+        $this->quantity = $quantity;
+    }
+
+    public function getOrderId()
+    {
+        return $this->order_id;
+    }
+
+    public function getProductId()
+    {
+        return $this->product_id;
+    }
+
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
