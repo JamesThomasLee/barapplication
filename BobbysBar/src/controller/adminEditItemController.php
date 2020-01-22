dc7747eb8576d6c8d240fa08fa263f899bef350b
James <james.lee-1@students.plymouth.ac.uk>
2020-01-22 21:00:55 +0000

    Added security validations to admin edit item.

--- BobbysBar/src/controller/adminEditItemController.php
+++ BobbysBar/src/controller/adminEditItemController.php
@@ -2,6 +2,7 @@
 session_start();
 include_once '../model/Menu_item.php';
 include_once '../model/DBContext.php';
+$errors = array();
 
 /*
  * The edit item controller is used to retrieve data from the edit item form and pass the data into an dbcontext update
@@ -13,20 +14,40 @@ include_once '../model/DBContext.php';
 
 if(isset($_POST["editItem"])){
     $product_id = $_SESSION['product_id'];
-    $product_name = $_POST['product_name'];
-    $product_supplier = $_POST['product_supplier'];
+    $product_name = trimInputs($_POST['product_name']);
+    $product_supplier = trimInputs($_POST['product_supplier']);
     $category = $_POST['category'];
-    $percentage = $_POST['percentage'];
-    $cost = $_POST['cost'];
+    $percentage = trimInputs($_POST['percentage']);
+    $cost = trimInputs($_POST['cost']);
 
-    $db = new DBContext();
-    //seperate update function for a bar snack due to data differing from a drink.
-    if($category == "Bar Snacks"){
-        $db->updateSnackItem($product_id, $product_name, $product_supplier, $cost);
-    }else{
-        $db->updateDrinkItem($product_id, $product_name, $product_supplier, $percentage, $cost);
+    //validate all inputs
+    //carry out validation checks
+    $errors = validateProductName($product_name, $errors);
+    $errors = validateProductSupplier($product_supplier, $errors);
+    //if category has a percentage (not bar snacks)
+    if($category != "Bar Snacks"){
+        $errors = validatePercentage($percentage, $errors);
     }
-    header("Location: ../../public/admin_pages/adminMenu.php");
+    $errors = validateCost($cost, $errors);
+    if(errors != null){
+        print_r($errors);
+        //back button
+        echo '<button onclick="history.back()">Go Back</button>';
+
+    }
+
+    //if there are no validation errors, proceed with item update.
+    if($errors == null){
+        $db = new DBContext();
+        //seperate update function for a bar snack due to data differing from a drink.
+        if($category == "Bar Snacks"){
+            $db->updateSnackItem($product_id, $product_name, $product_supplier, $cost);
+        }else{
+            $db->updateDrinkItem($product_id, $product_name, $product_supplier, $percentage, $cost);
+        }
+        header("Location: ../../public/admin_pages/adminMenu.php");
+    }
+
 }
 
 //used to change the sale status of an item
@@ -40,6 +61,62 @@ if(isset($_POST["change_status"])){
 //cancel edit and return to menu without saving.
 if(isset($_POST['cancel'])){
     header("Location: ../../public/admin_pages/adminMenu.php");
+}
+
+//strip inputs
+function trimInputs($input){
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+}
+
+//check product name for empty field or string too long
+function validateProductName($productName, $errors){
+    if(empty($productName)){
+        array_push($errors, "*Please enter a product name.");
+    }else{
+        if(strlen($productName) > 29){
+            array_push($errors, "*Product name too long");
+        }
+    }
+    return $errors;
+}
+
+//check product supplier for empty field or string too long
+function validateProductSupplier($productSupplier, $errors){
+    if(empty($productSupplier)){
+        array_push($errors, "*Please enter a product supplier.");
+    }else{
+        if(strlen($productSupplier) > 39){
+            array_push($errors, "*Product supplier too long");
+        }
+    }
+    return $errors;
+}
+
+//check product percentage for empty field, or non numeric
+function validatePercentage($percentage, $errors){
+    if(empty($percentage)){
+        array_push($errors, "*Please enter a percentage");
+    }else{
+        if(!is_numeric($percentage)){
+            array_push($errors, "*Please enter a valid percentage");
+        }
+    }
+    return $errors;
+}
+
+//check product cost for empty field, or non numeric
+function validateCost($cost, $errors){
+    if(empty($cost)){
+        array_push($errors, "*Please enter a cost");
+    }else{
+        if(!is_numeric($cost)){
+            array_push($errors, "*Please enter a valid cost");
+        }
+    }
+    return $errors;
 }
 
 ?>
